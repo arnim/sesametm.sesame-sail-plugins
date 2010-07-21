7c0ebb1ed286a2df083928896d9197e3bd4cb6c5
arnim.bleier <arnim.bleier@localhost>
2010-07-21 15:11:00 +0200

    first working version

--- maiana-seealso-handler/src/test/java/de/topicmapslab/sesame/sail/tmapi/TestMaianaSeeAlsoHandler.java
+++ maiana-seealso-handler/src/test/java/de/topicmapslab/sesame/sail/tmapi/TestMaianaSeeAlsoHandler.java
@@ -5,13 +5,12 @@
 
 package de.topicmapslab.sesame.sail.tmapi;
 
+import junit.framework.TestCase;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.model.Statement;
+import org.openrdf.model.vocabulary.RDFS;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryResult;
@@ -24,21 +23,18 @@ import org.tmapi.core.TopicMapSystemFactory;
 
 /**
  * @author Arnim Bleier
- *
+ * 
  */
-public class TestMaianaSeeAlsoHandler {
-	
-	
+public class TestMaianaSeeAlsoHandler extends TestCase {
+
 	private static TmapiStore _sail;
 	private static Repository _tmapiRepository;
 	private static RepositoryConnection _con;
 	private static TopicMapSystem _tms;
 
 	final static String baseIRI = "http://www.topicmapslab.de/test/base/";
-	
-	
 
-
+	@Override
 	@Before
 	public void setUp() throws Exception {
 		_tms = TopicMapSystemFactory.newInstance().newTopicMapSystem();
@@ -79,24 +75,178 @@ public class TestMaianaSeeAlsoHandler {
 	
 	@Test
 	public void testxxO() throws Exception {
-		
-		
+		int i = 0;
 		RepositoryResult<Statement> result;
 		
 		
 		result = _con.getStatements( null,
 				null, 
-				null,
-				true);
-				
-		System.out.println(result.asList().size());
+				_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/t/http://www.topicmapslab.de/test/base/bertsi1"),
+						true);
+		assertTrue(result.hasNext());
+		Statement statement = result.next();
+		if (statement.getPredicate().equals(RDFS.SEEALSO))
+			i++;
+		assertEquals("http://www.topicmapslab.de/test/base/bertsi1", statement
+				.getSubject().stringValue());
+		assertEquals("http://www.topicmapslab.de/test/base/t/http://www.topicmapslab.de/test/base/bertsi1", statement
+				.getObject().stringValue());
+		assertFalse(result.hasNext());
+		assertEquals(1, i);
+		
+		result = _con.getStatements( null,
+				null, 
+				_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/t/http://www.topicmapslab.de/test/base/bertsi_wrong"),
+						true);
+		assertFalse(result.hasNext());
+		
+		result = _con.getStatements( null,
+				null, 
+				_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test_wrong/base/t/http://www.topicmapslab.de/test/base/bertsi"),
+						true);
+		assertFalse(result.hasNext());
+		
+		
+		result = _con.getStatements( null,
+				null, 
+				_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/bertsi1"),
+						true);
+		assertEquals(1, result.asList().size());
+
+	}
+	
+	
+	
+	
+	@Test
+	public void testxPx() throws Exception {
+		RepositoryResult<Statement> result;
+		
+		
+		result = _con.getStatements(null, RDFS.SEEALSO,
+				null, true);
+		assertTrue(result.hasNext());
+		assertEquals(RDFS.SEEALSO, result.next().getPredicate());
+		
+		assertTrue(result.hasNext());
+		assertEquals(RDFS.SEEALSO, result.next().getPredicate());
+		
+		assertTrue(result.hasNext());
+		assertEquals(RDFS.SEEALSO, result.next().getPredicate());
+		
+		assertTrue(result.hasNext());
+		assertEquals(RDFS.SEEALSO, result.next().getPredicate());
+		
+		assertTrue(result.hasNext());
+		assertEquals(RDFS.SEEALSO, result.next().getPredicate());
+		
+		assertTrue(result.hasNext());
+		assertEquals(RDFS.SEEALSO, result.next().getPredicate());
+
+		assertFalse(result.hasNext());
+		
+		result = _con.getStatements(null, RDFS.LABEL,
+				null, true);
+		assertFalse(result.hasNext());
+		
+		
+		result = _con.getStatements(null, _con.getValueFactory().createURI(baseIRI + "employee"),
+				null, true);
+		
+		assertEquals(2, result.asList().size());
+
+	}
+	
+	
+	
+	@Test
+	public void testSxx() throws Exception {
+		int i = 0;
+		RepositoryResult<Statement> result = _con.getStatements(_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/bertsi1"), null,
+				null, true);
+		assertTrue(result.hasNext());
+		Statement statement = result.next();
+		if (statement.getPredicate().equals(RDFS.SEEALSO)){
+			assertEquals("http://www.topicmapslab.de/test/base/t/http://www.topicmapslab.de/test/base/bertsi1", statement
+					.getObject().stringValue());
+			i++;
+			
+		}
+
+		assertEquals("http://www.topicmapslab.de/test/base/bertsi1", statement
+				.getSubject().stringValue());
+
+		assertTrue(result.hasNext());
+		statement = result.next();
+		if (statement.getPredicate().equals(RDFS.SEEALSO))
+			i++;
+		assertEquals("http://www.topicmapslab.de/test/base/bertsi1", statement
+				.getSubject().stringValue());
+		assertFalse(result.hasNext());
+		assertEquals(1, i);
+		
+		result = _con.getStatements(_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/bertsi_wrong"), null,
+				null, true);
+		assertFalse(result.hasNext());
+
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void testSPx() throws Exception {
+		RepositoryResult<Statement> result = _con.getStatements(_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/bertsi1"), RDFS.SEEALSO,
+				null, true);
+		assertTrue(result.hasNext());
+		Statement statement = result.next();
+		assertEquals(RDFS.SEEALSO, statement.getPredicate());
+		assertFalse(result.hasNext());
+		result = _con.getStatements(_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/bertsi6"), RDFS.SEEALSO,
+				null, true);
+		assertFalse(result.hasNext());
+		_con.getStatements(_con
+				.getValueFactory().createURI(
+						"http://www.topicmapslab.de/test/base/bertsi1"), RDFS.CONTAINER,
+				null, true);
+		assertFalse(result.hasNext());
 	}
+
+
+
+	@Test
+	public void testxxx() throws Exception {
+		RepositoryResult<Statement> result = _con.getStatements(null, null,
+				null, true);
+		assertEquals(10, result.asList().size());
+		result = _con.getStatements(null, null,
+				null, true);
+		int i = 0;
+		while (result.hasNext()) {
+			Statement statement = result.next();
+			if (statement.getPredicate().equals(RDFS.SEEALSO)){
+				i++;
+			}
+				
+		}
+		assertEquals(6, i);
+	}
+
+
+	
+	
 
 }
